a4e3800447421416a103a3ea585ccafeb7623e97
Camilo Rodriguez <crodrigr@gmail.com>
2024-03-13 20:33:25 -0500

    configuración bd

--- src/main/java/com/campusland/crudcliente/repositories/entities/Cliente.java
+++ src/main/java/com/campusland/crudcliente/repositories/entities/Cliente.java
@@ -2,6 +2,8 @@ package com.campusland.crudcliente.repositories.entities;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,11 +12,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="clientes")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Cliente {
     
     @Id
@@ -25,8 +29,9 @@ public class Cliente {
     @Column(nullable = false)
     private String apellido;
     @Column(nullable = false,unique = true)
-    private String email;
+    private String email;   
     @Column(name="create_at")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date   createAt; 
 
     
